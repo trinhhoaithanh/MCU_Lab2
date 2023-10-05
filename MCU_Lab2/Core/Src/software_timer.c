a28b393929b286c1f0f317165c91d7ee70b02708
ththanh <hoaithanhtrinh2002@gmail.com>
2023-10-05 13:21:47 +0700

    exercise 1

---
+++ MCU_Lab2/Core/Src/software_timer.c
@@ -0,0 +1,24 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Sep 24, 2022
+ *      Author: KAI
+ */
+
+#include "software_timer.h"
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+void setTimer1(int duration){
+	timer1_counter = duration;
+	timer1_flag = 0;
+}
+void timerRun(){
+	if(timer1_counter > 0){
+		timer1_counter--;
+		if(timer1_counter <= 0){
+			timer1_flag = 1;
+		}
+	}
+}
